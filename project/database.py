46968787ef8946761d0717baa9d645900a652d08
suusuu00 <josuna0114@gmail.com>
2024-01-15 20:26:35 +0900

    database.py config modify

--- project/database.py
+++ project/database.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+import json
+
+with open("config.json", "r") as config_file:
+    config_data = json.load(config_file)
 
 # PostgreSQL 연결 정보
-DATABASE_USER = "manager"
-DATABASE_PASSWORD = "acquery22"
-DATABASE_HOST = "localhost"
-DATABASE_PORT = "5432"
-DATABASE_NAME = "acquery"
+DATABASE_USER = config_data["DATABASE_USER"]
+DATABASE_PASSWORD = config_data["DATABASE_PASSWORD"]
+DATABASE_HOST = config_data["DATABASE_HOST"]
+DATABASE_PORT = config_data["DATABASE_PORT"]
+DATABASE_NAME = config_data["DATABASE_NAME"]
 
 # PostgreSQL 연결 문자열
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
