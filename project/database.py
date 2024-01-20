e0231bf7242a88d391d3d7340ba8582cf7a6445e
suusuu00 <josuna0114@gmail.com>
2024-01-20 16:29:55 +0900

    비동기처리

--- project/database.py
+++ project/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 import json
@@ -15,12 +16,26 @@ DATABASE_PORT = config_data["DATABASE_PORT"]
 DATABASE_NAME = config_data["DATABASE_NAME"]
 
 # PostgreSQL 연결 문자열
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 # SQLAlchemy 엔진 생성
+'''
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
+'''
+
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+
 
 # SQLAlchemy 세션 생성
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+async_session = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
--- project/main.py
+++ project/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Request, HTTPException, Depends
 from fastapi.templating import Jinja2Templates
-from database import SessionLocal
+from database import async_session
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.staticfiles import StaticFiles
 import pandas as pd
 
@@ -9,12 +10,13 @@ app = FastAPI()
 templates = Jinja2Templates(directory = "templates")
 app.mount("/static", StaticFiles(directory="templates"), name="static")
 
-def get_db():
-    db = SessionLocal()
+async def get_db() -> AsyncSession:
     try:
-        yield db 
+        async with async_session() as db:
+            yield db
     finally:
-        db.close()
+        if db:
+            await db.close()
 
 # nl2sql 모델을 이용하여 질문에 해당하는 쿼리문 가져오는 함수
 def generate_sql_query(question: str):
@@ -26,16 +28,18 @@ def generate_sql_query(question: str):
 
 
 # 쿼리문에 해당하는 데이터표 가져오는 함수
-def execute_sql_query(db: Session, sql_query: str):
+async def execute_sql_query(db: AsyncSession, sql_query: str):
     try:
         # SQL 쿼리 실행
-        result = db.execute(sql_query)
+        result = await db.execute(sql_query)
 
         # 결과를 DataFrame으로 변환
         df = pd.DataFrame(result.fetchall(), columns=result.keys())
 
-        # 행별로 딕셔너리 변환
-        return df.to_dict(orient="records")
+        # Null 값 처리 후 행별로 딕셔너리 변환
+        records = df.where(pd.notna(df), None).to_dict(orient="records")
+
+        return records
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error executing SQL query: {str(e)}")
@@ -56,7 +60,7 @@ def read_homepage(request: Request):
 
 # 새 게시판 글 저장
 @app.post("/acquery/question/")
-async def new_question(request: Request, db: Session = Depends(get_db)):
+async def new_question(request: Request, db: AsyncSession = Depends(get_db)):
     try:
         data = await request.json()
         question = data["question"]
@@ -64,8 +68,7 @@ async def new_question(request: Request, db: Session = Depends(get_db)):
         # nl2sql 모델로 question을 보내서 쿼리문을 받아옴
         sql_query = generate_sql_query(question)
         # 쿼리문을 현재 db에 적용하여 해당 데이터표 가져옴
-        data_dict = execute_sql_query(db, sql_query)
-
+        data_dict = await execute_sql_query(db, sql_query)
         # result값을 적절한 json(딕셔너리) 형식으로 변환하여 보내줌
         result = {
             'query': sql_query,
