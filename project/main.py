a445246445606867be215e7bb1788f83e1164401
suusuu00 <josuna0114@gmail.com>
2024-01-27 17:12:59 +0900

    streamlit에 맞게 수정

--- project/main.py
+++ project/main.py
@@ -5,8 +5,25 @@ from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.staticfiles import StaticFiles
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS 설정
+origins = [
+    "http://localhost",  # Streamlit 앱이 실행 중인 주소
+    "http://localhost:8000",  # Streamlit 앱이 실행 중인 주소와 포트
+]
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 실제 사용할 때는 특정 origin을 명시하는 것이 좋습니다.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 templates = Jinja2Templates(directory = "templates")
 app.mount("/static", StaticFiles(directory="templates"), name="static")
 
@@ -49,21 +66,17 @@ async def execute_sql_query(db: AsyncSession, sql_query: str):
 
 ############# 백엔드 API #############
 # 파이썬 강좌 게시판 전체 게시물 보기
-@app.get("/acquery/")
-def read_homepage(request: Request):
-        
-        return templates.TemplateResponse(
-            "web4.html", {
-                "request": request
-            }
-        )
+@app.get("/")
+def read_homepage():
+    return None
+    
 
-# 새 게시판 글 저장
-@app.post("/acquery/question/")
-async def new_question(request: Request, db: AsyncSession = Depends(get_db)):
+# 대화문 받아 쿼리문과 데이터 반환
+@app.post("/question/")
+async def new_question(question: dict, db: AsyncSession = Depends(get_db)):
     try:
-        data = await request.json()
-        question = data["question"]
+        #data = await request.json()
+        question = question["question"]
 
         # nl2sql 모델로 question을 보내서 쿼리문을 받아옴
         sql_query = generate_sql_query(question)
